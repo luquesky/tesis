366b322fdff02c66d26f71500bc0dd5202a83ebf
Juan Manuel Pérez <jmperez.85@gmail.com>
2014-09-24 00:03:52 -0300

    Add command to extract features.

--- main.py
+++ main.py
@@ -4,8 +4,10 @@
 Usage:
   main.py tama <path_to_wav_file> <feature>
   main.py hybrid_tama <path_to_wav_file> <feature>
+  main.py extract_features <path_to_wav_file> <start> <end>
 """
 from docopt import docopt
+from sympy import Interval
 from speech import tama, hybrid_tama, SpeechBuilder
 
 def run_tama(arguments):
@@ -18,10 +20,19 @@ def run_hybrid_tama(arguments):
     speech = SpeechBuilder(arguments["<path_to_wav_file>"]).speech
     print tama(speech, feature)
 
+def extract_features(arguments):
+    speech = SpeechBuilder(arguments["<path_to_wav_file>"]).speech
+    interval = Interval(float(arguments["<start>"]), float(arguments["<end>"]))
+    print speech.get_features(interval)
+
+
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='Tesis 0.0.1')
 
     if arguments["tama"]:
         run_tama(arguments)
-    if arguments["hybrid_tama"]:
+    elif arguments["hybrid_tama"]:
         run_hybrid_tama(arguments)
+    elif arguments["extract_features"]:
+        extract_features(arguments)
+
