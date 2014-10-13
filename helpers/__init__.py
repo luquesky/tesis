73bce141a459c9e86783c9c69c3214492be13108
jmperez <jmperez.85@gmail.com>
2014-10-13 02:05:34 -0300

    Add interval distance.

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -7,3 +7,12 @@ def autocorrelation_coefficient(X, lag):
     num = sum((X[lag:] - xm) * (X[:-lag] - xm))
     denom = ss(X - xm)
     return num / denom
+
+def interval_distance(int1, int2):
+    intersect = int1.closure.intersect(int2.closure)
+
+    # If not empty =>
+    if not intersect.is_EmptySet:
+        return 0.0
+    else:
+        return min([abs(int2.inf - int1.sup), abs(int1.inf - int2.sup)])
--- helpers/test.py
+++ helpers/test.py
@@ -1,10 +1,27 @@
 #! coding:utf-8
 import numpy as np
 from unittest import TestCase
-from . import autocorrelation_coefficient
+from sympy import Interval
+from . import autocorrelation_coefficient, interval_distance
 
 class AutocorrelationCoefficientTest(TestCase):
     def test_it_works(self):
         x = np.array([1.0, 2.0, 3.0, 4.0], dtype=float)
 
         self.assertAlmostEqual(autocorrelation_coefficient(x, lag=1), 0.25)
+
+class IntervalDistanceTest(TestCase):
+    def test_for_overlapping_intervals(self):
+        interval1 = Interval(0, 1)
+        interval2 = Interval(1, 2)
+        self.assertAlmostEqual(interval_distance(interval1, interval2), .0)
+
+    def test_for_first_interval_is_to_the_left(self):
+        interval1 = Interval(0, 1)
+        interval2 = Interval(2, 3)
+        self.assertAlmostEqual(interval_distance(interval1, interval2), 1.0)
+
+    def test_for_first_interval_is_to_the_right(self):
+        interval1 = Interval(2, 3)
+        interval2 = Interval(0, 1)
+        self.assertAlmostEqual(interval_distance(interval1, interval2), 1.0)
