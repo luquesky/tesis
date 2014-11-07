ac2af4016e317606954cb138399ba4c6d8572c1f
jmperez <jmperez.85@gmail.com>
2014-11-07 00:22:29 -0300

    Move analysis to session model.

--- main.py
+++ main.py
@@ -4,9 +4,9 @@
 Usage:
   main.py tama <path_to_wav_file> <feature>
   main.py hybrid_tama <path_to_wav_file> <feature>
+  main.py save_images <path_to_wav_file>
   main.py compare <feature> <path_to_wav1> <path_to_wav2>
   main.py extract_features <path_to_wav_file> <start> <end>
-
 """
 import config
 import matplotlib.pyplot as plt
@@ -48,4 +48,6 @@ if __name__ == '__main__':
         extract_features(arguments)
     elif arguments["compare"]:
         compare(arguments)
+    elif arguments["save_images"]:
+        save_images(arguments)
 
--- session/session.py
+++ session/session.py
@@ -1,6 +1,24 @@
 #! coding:utf-8
+import logging
+from tama import hybrid_tama
+
+logger = logging.getLogger('main')
+
 class Session(object):
     def __init__(self, tasks, speechA, speechB):
         self.tasks = tasks
         self.speechA = speechA
         self.speechB = speechB
+
+    # Analyze with tama each task of the session
+    def analyze(self, features=[]):
+        tamas = {}
+        for task in self.tasks:
+            tamas[task] = {}
+            print "Working in task: %s" % task
+            for feature in features:
+                tamas[task][feature] = {}
+                print "Calculating %s" % feature
+                tamas[task][feature]["A"] = hybrid_tama(self.speechA, feature, interpolate=False, interval=task.interval)
+                tamas[task][feature]["B"] = hybrid_tama(self.speechB, feature, interpolate=False, interval=task.interval)
+        return tamas
