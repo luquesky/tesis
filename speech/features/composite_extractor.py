66a1cd39423e36c20fb87bb5b7e7611a84a245e4
jmperez <jmperez.85@gmail.com>
2014-11-24 13:35:12 -0300

    Composite extractor now allows more than two extractors.

--- speech/features/composite_extractor.py
+++ speech/features/composite_extractor.py
@@ -1,6 +1,5 @@
 #! coding:utf-8
-# An object of this class has the responsibility of extracting the features (f_0, ) out of a wav file
-# It should be created with both a path to the wav file, and to a praat script which returns the features (in the project, they are at scripts/)
+# This extractor combines several extractors into one
 import logging
 
 logger = logging.getLogger('main')
@@ -8,15 +7,14 @@ logger = logging.getLogger('main')
 class CompositeExtractor(object):
 
     # All paths should be absolute...praat
-    def __init__(self, extractor1, extractor2):
-        self.extractor1 = extractor1
-        self.extractor2 = extractor2
+    def __init__(self, *extractors):
+        self.extractors = extractors
 
     def extract_features(self, interval):
-        features1 = self.extractor1.extract_features(interval)
-        features2 = self.extractor2.extract_features(interval)
+        features = {}
 
-        features = features1.copy()
-        features.update(features2)
+        for extractor in self.extractors:
+            new_features = extractor.extract_features(interval)
+            features.update(new_features)
 
         return features
