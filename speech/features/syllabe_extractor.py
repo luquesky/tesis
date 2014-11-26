b0d65dec43de077e1d3c523c368762a02886f93c
jmperez <jmperez.85@gmail.com>
2014-11-25 23:11:06 -0300

    Extract syllabes more efficiently.

--- speech/features/syllabe_extractor.py
+++ speech/features/syllabe_extractor.py
@@ -1,9 +1,11 @@
 #! coding:utf-8
+import bisect
 
 class SyllabeExtractor(object):
     def __init__(self, word_intervals, syllabes_count):
         self.word_intervals = word_intervals
         self.syllabes_count = syllabes_count
+        self.supremes = [wint.sup for wint in self.word_intervals]
 
     def extract_features(self, interval):
         count = self.count_syllabes(interval)
@@ -15,9 +17,20 @@ class SyllabeExtractor(object):
 
     def count_syllabes(self, interval):
         count = 0
+        index = bisect.bisect_left(self.supremes, interval.inf)
 
-        for word_interval in self.word_intervals:
-            if not word_interval.intersect(interval.interior).is_EmptySet:
+        if index == len(self.word_intervals):
+            return 0
+
+        while index < len(self.word_intervals):
+            word_interval = self.word_intervals[index]
+
+            intersection = word_interval.intersect(interval.closure)
+
+            if intersection.measure > 0:
                 count += self.syllabes_count[word_interval.word]
+            elif intersection.is_EmptySet:
+                break
+            index+=1
 
         return count
