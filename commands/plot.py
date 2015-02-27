8aa38a818a203502c88c3d78cefeac857d6fd671
jmperez <jmperez.85@gmail.com>
2015-02-27 20:35:29 -0300

    New notebook

--- commands/plot.py
+++ commands/plot.py
@@ -1,6 +1,8 @@
 #! coding: utf-8
 import logging
+import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
+
 from scipy.stats.stats import pearsonr
 from helpers import autocorrelation_coefficient
 from tama import tama, hybrid_tama
@@ -17,21 +19,26 @@ def plot_tama(speech, feature, hybrid=True):
     plt.show()
 
 def plot_tamas(feature, TA, averagesA, TB, averagesB):
-    plt.plot(TA, averagesA)
-    plt.plot(TB, averagesB)
+    plt.plot(TA, averagesA, c="r")
+    plt.plot(TB, averagesB, c="b")
 
     plt.xlabel("Time")
     plt.ylabel("Average %s" % feature)
 
     plt.show()
 
-    lags = range(1, 10)
+def plot_autocorrelations(feature, TA, averagesA, TB, averagesB):
+    lags = range(1, min(10, len(TA)))
     autocorr1 = [autocorrelation_coefficient(averagesA, lag) for lag in lags]
 
     autocorr2 = [autocorrelation_coefficient(averagesB, lag) for lag in lags]
 
-    plt.scatter(lags, autocorr1, c="g")
-    plt.scatter(lags, autocorr2, c="r")
+    red_patch = mpatches.Patch(color='red', label='Speaker A')
+    blue_patch = mpatches.Patch(color='blue', label='Speaker B')
+    plt.legend(handles=[red_patch, blue_patch])
+
+    plt.scatter(lags, autocorr1, c="red")
+    plt.scatter(lags, autocorr2, c="blue")
 
     plt.show()
 
