92894ef57f6ffe001ab9c8342ea494f0f86054ba
jmperez <jmperez.85@gmail.com>
2015-03-02 19:53:37 -0300

    Let speechs have interval so they are narrowed to a piece of the audio.

--- speech/speech.py
+++ speech/speech.py
@@ -2,7 +2,7 @@
 from helpers import build_utterances
 
 class Speech(object):
-    def __init__(self, path_to_wav, word_intervals, feature_extractor=None):
+    def __init__(self, path_to_wav, interval, word_intervals, feature_extractor=None):
         self.path_to_wav = path_to_wav
         self.length = word_intervals[-1].sup
         self.utterances = build_utterances(word_intervals)
