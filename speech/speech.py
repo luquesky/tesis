ddef2f2bbe9cc045bb513ea0a734cc5c2c5869f0
jmperez <jmperez.85@gmail.com>
2015-03-15 18:37:49 -0300

    Add method to check if it is speaking.

--- speech/speech.py
+++ speech/speech.py
@@ -1,4 +1,5 @@
 #! coding:utf-8
+from bisect import bisect_left
 from helpers import build_utterances
 
 class Speech(object):
@@ -6,8 +7,20 @@ class Speech(object):
         self.path_to_wav = path_to_wav
         self.length = word_intervals[-1].sup
         self.utterances = build_utterances(word_intervals)
+        # TODO: Try to refactor this
+        self.supremes = [utt.sup for utt in self.utterances]
+
         self.feature_extractor = feature_extractor
 
     def get_features(self, interval):
         return self.feature_extractor.extract_features(interval)
 
+    def is_speaking_at(self, time):
+        index = bisect_left(self.supremes, time)
+
+        if index == len(self.supremes):
+            return False
+
+        utterance = self.utterances[index]
+
+        return utterance.contains(time)
--- speech/tests/speech_test.py
+++ speech/tests/speech_test.py
@@ -6,9 +6,6 @@ from .. import Speech, WordInterval
 
 class SpeechTest(TestCase):
 
-    def setUp(self):
-        pass
-
     def test_length_returns_length_of_the_speech(self):
         speech = Speech(path_to_wav="", interval=Interval(0, 15),word_intervals=[
             WordInterval(0, 14, "#"),
@@ -30,3 +27,27 @@ class SpeechTest(TestCase):
 
         mock_extractor.extract_features.assert_called_with(interval)
 
+
+    def test_is_speaking_returns_false_when_silent_interval(self):
+        mock_extractor = MagicMock(spec=["extract_features"])
+        speech = Speech(path_to_wav="",
+            interval=Interval(0, 15),
+            word_intervals=[
+                WordInterval(0, 14, "#"),
+                WordInterval(14, 14.5, "hi"),
+                WordInterval(14.5, 15.5, "frank")],
+            feature_extractor= mock_extractor)
+
+        self.assertFalse(speech.is_speaking_at(10.0))
+
+    def test_is_speaking_returns_true_when_not_silent_interval(self):
+        mock_extractor = MagicMock(spec=["extract_features"])
+        speech = Speech(path_to_wav="",
+            interval=Interval(0, 15),
+            word_intervals=[
+                WordInterval(0, 14, "#"),
+                WordInterval(14, 14.5, "hi"),
+                WordInterval(14.5, 15.5, "frank")],
+            feature_extractor= mock_extractor)
+
+        self.assertTrue(speech.is_speaking_at(14.25))
