3e95a3920031784b9b8ac4185764bf2895f4c70f
jmperez <jmperez.85@gmail.com>
2015-06-11 19:36:07 -0300

    Updated plots.

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,4 +1,40 @@
 #! coding:utf-8
+import numpy as np
+import pandas as pd
+from tsa import entrainment, cross_correlogram
+
+
+def plot_tamas(A, B, feature, ax=None):
+    sp = pd.DataFrame({'A': A, 'B': B}).plot(color=['r', 'b'], style='-o', ax=ax)
+    sp.set_xlabel("Time")
+    sp.set_ylabel("Average %s" % feature)
+
+    l_AB, E_AB, l_BA, E_BA = entrainment(A, B, lags=range(-6, 6))
+    print "A -> B = %.5f (lag = %s)" % (E_AB, l_AB)
+    print "B -> A = %.5f (lag = %s)" % (E_BA, l_BA)
+
+
+def plot_correlograms(A, B, feature, ax=None):
+    n = len(A)
+    lag_limit = min(6, n)
+    lags = range(-lag_limit, lag_limit+1)
+
+    cross_correlations = cross_correlogram(A, B, lags)
+    sp = cross_correlations.plot(style="bo", ax=ax)
+    sp.set_xlabel("Lag")
+    sp.set_ylabel("Correlation")
+
+    confidence_value = 2 / np.sqrt(n)
+    # Draw confidence interval
+    sp.axhline(confidence_value, linestyle='dashed', color='g')
+    sp.axhline(-confidence_value, linestyle='dashed', color='g')
+    # Draw axes
+    sp.axvline(0, color="k")
+    sp.axhline(0, color="k")
+    # Draw max values
+    l_AB, E_AB, l_BA, E_BA = entrainment(A, B, lags=range(-6, 6))
+    pd.Series({l_AB: E_AB, l_BA: E_BA}).plot(style="ro", ax=ax)
+
 
 
 def interval_distance(int1, int2):
