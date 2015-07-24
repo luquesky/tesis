0622d1af5d9e2257e3bcf967489bd5fda250776f
jmperez <jmperez.85@gmail.com>
2015-07-24 17:40:32 -0300

    Fix issue with JOIN

--- analyzers/analyzer.py
+++ analyzers/analyzer.py
@@ -4,8 +4,10 @@ import pandas as pd
 from tama import tama, normalize
 from tsa import entrainment, autoregressive_prewhitening
 
+
 class TaskTooShort(Exception):
     pass
+
 
 class Analyzer(object):
 
@@ -13,16 +15,16 @@ class Analyzer(object):
         self.feature = feature
 
     def analyze(self, sessions):
-        session_table = pd.DataFrame(columns=self.__columns__())
+        session_table = pd.DataFrame(columns=self.__columns__(), dtype=float)
 
         for session in sessions:
             for task in session.tasks:
                 try:
                     entry_name = "S%s-T%s" % (session.number, task.number)
                     session_table.loc[entry_name+"-A"], session_table.loc[entry_name+"-B"] = self.__analyze_task(session, task)
-                except TaskTooShort as e:
+                except TaskTooShort:
                     continue
-        return session_table
+        return session_table.astype(float)
 
     def __generate_task_series(self, A, B, meanA, meanB, session, task):
         l_AB, E_AB, l_BA, E_BA = entrainment(A, B, lags=range(-6, 6))
@@ -30,28 +32,26 @@ class Analyzer(object):
         AB_data = pd.Series({
             "session": session.number,
             "task": task.number,
-            "speaker": "A",
+            "speaker": 0,
             "count": A.count(),
-            "entrainment" : E_AB,
-            "best_lag" : abs(l_AB),
+            "entrainment": E_AB,
+            "best_lag": abs(l_AB),
             "tama_mean": meanA,
         })
 
         BA_data = pd.Series({
             "session": session.number,
             "task": task.number,
-            "speaker": "B",
+            "speaker": 1,
             "count": B.count(),
-            "entrainment" : E_BA,
-            "best_lag" : abs(l_BA),
+            "entrainment": E_BA,
+            "best_lag": abs(l_BA),
             "tama_mean": meanB,
         })
 
         return AB_data, BA_data
 
     def __analyze_task(self, session, task):
-        task_data = pd.Series()
-
         A, meanA = tama(task.speechA, self.feature)
         B, meanB = tama(task.speechB, self.feature)
 
@@ -62,7 +62,6 @@ class Analyzer(object):
         nB = normalize(B, meanB)
 
         rA, rB = autoregressive_prewhitening(nA, nB)
-        task_key = task.name.replace("Task", "T").replace(" ", "")
 
         return self.__generate_task_series(rA, rB, meanA=meanA, meanB=meanB, session=session, task=task)
 
@@ -73,4 +72,4 @@ class Analyzer(object):
             "speaker",
             "tama_mean",
             "entrainment",
-            "best_lag" ]
+            "best_lag"]
