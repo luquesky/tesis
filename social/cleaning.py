36b9aafb72bf1184995ef3f3983d4573f13c20bd
Juan Manuel Pérez <jmperez.85@gmail.com>
2015-11-10 19:37:42 -0300

    Remove no variables

--- social/cleaning.py
+++ social/cleaning.py
@@ -11,7 +11,10 @@ def remove_yes_no_variables(social_dataframe):
     no_variables = __variables_with_suffix(social_dataframe, "no")
     yes_variables = __variables_with_suffix(social_dataframe, "yes")
 
+    # Remove
     yes_rename = {v:v[0:-4] for v in yes_variables}
     new_df = social_dataframe.rename(columns=yes_rename, inplace=False)
+
+    new_df.drop(no_variables, axis=1, inplace=True)
 
     return new_df
