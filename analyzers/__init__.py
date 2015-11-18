4fe75b9951456db323c264db971ba9098801684e
jmperez <jmperez.85@gmail.com>
2015-11-18 01:27:09 -0300

    Add full analysis

--- analyzers/__init__.py
+++ analyzers/__init__.py
@@ -1,2 +1,13 @@
 #! coding:utf-8
+import pandas as pd
+import social
 from analyzer import Analyzer
+
+
+def correlation_analysis(df, entrainment_var="entrainment"):
+    corr = pd.Series(index=social.relevant_variables)
+
+    for var in social.relevant_variables:
+        corr[var] = df[entrainment_var].corr(df[var])
+
+    return corr
