ab0f2f014abb57e0a5286d2bafa2bd63a880f274
jmperez <jmperez.85@gmail.com>
2015-11-21 21:17:45 -0300

    Add experimental notebook

--- analyzers/__init__.py
+++ analyzers/__init__.py
@@ -4,10 +4,16 @@ import social
 from analyzer import Analyzer
 
 
-def correlation_analysis(df, entrainment_var="entrainment"):
-    corr = pd.Series(index=social.relevant_variables)
+def correlation_analysis(df, entrainment_var="entrainment", social_vars=None):
 
-    for var in social.relevant_variables:
-        corr[var] = df[entrainment_var].corr(df[var])
+    social_vars = social_vars or social.relevant_variables
+
+    corr = pd.Series(index=social_vars)
+
+    for var in social_vars:
+        try:
+            corr[var] = df[entrainment_var].corr(df[var])
+        except KeyError as e:
+            import ipdb; ipdb.set_trace()
 
     return corr
