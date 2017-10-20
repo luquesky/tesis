e17a45779085e9833e0a7b962bc76faf8dc55251
Juan Manuel Pérez <jmperez.85@gmail.com>
2017-10-20 14:18:07 -0300

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,20 @@
+#! coding: utf-8
+from setuptools import setup, find_packages
+
+setup(
+    name='games_entrainment',
+    version='0.1.0',
+    author="Juan Manuel Pérez",
+    author_email="jmperez.85@gmail.com",
+    url="https://github.com/geekazoid/tesis",
+    packages=['games_entrainment'],
+    install_requires=[
+        "configparser == 3.5.0",
+        "dill == 0.2.2",
+        "fire == 0.1.1",
+        "numpy == 1.10.1",
+        "pandas == 0.17.1",
+        "scipy==0.16.1",
+        "sympy==0.7.5",
+    ],
+)
